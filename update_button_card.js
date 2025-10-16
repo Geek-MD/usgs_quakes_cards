c3ded94129790b1a85fee2496e599fc0d352f05b
Edison Montes M. <edison.montes@gmail.com>
2025-10-16 17:55:02 -0300

    Update update_button_card.js

--- update_button_card.js
+++ update_button_card.js
@@ -1,21 +1,37 @@
 class UsgsQuakesUpdateButton extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
   set hass(hass) {
     this.hass = hass;
-    this.innerHTML = `
+    this.render();
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = `
       <ha-card>
         <div style="padding: 16px;">
-          <mwc-button raised @click="${() => this.updateFeed()}">Update USGS Feed</mwc-button>
+          <mwc-button raised id="updateBtn">Update USGS Feed</mwc-button>
         </div>
       </ha-card>
     `;
+
+    this.shadowRoot.getElementById("updateBtn")?.addEventListener("click", () => this.updateFeed());
   }
 
   updateFeed() {
+    if (!this.hass) {
+      console.error("Home Assistant instance not available.");
+      return;
+    }
+
     this.hass.callService("usgs_quakes", "force_feed_update", {});
   }
 
   setConfig(config) {
-    this.config = config;
+    this.config = config || {};
   }
 
   getCardSize() {
